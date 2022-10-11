5ed0988b0911fbd4869fde9102a3d597a300229f
Bruno Mota <bruno@GESTSING.BR>
2022-10-11 01:01:17 -0300

    evolução

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
-module.exports = function(a){
-	return a * 2;
+const amqplib = require('amqplib');
+const publisher  = require('./publisher');
+
+const AMQP_URL = process.env.AMQP_URL;
+const AMQP_QUEUE = process.env.AMQP_QUEUE;
+
+async function processMessage(msg, channel, callback) {
+
+  try {
+
+    let payload = JSON.parse( msg.content.toString() );  
+    
+	let payload_retorno = (callback) ? await callback( payload ) : null;
+
+    console.log('payload_retorno', payload_retorno );
+    console.log('process.env.AMQP_WEBHOOK_QUEUE', process.env.AMQP_WEBHOOK_QUEUE );
+    console.log('process.env.AMQP_QUEUE_PUBLISHER', process.env.AMQP_QUEUE_PUBLISHER );
+
+    
+    if ( process.env.AMQP_WEBHOOK_QUEUE && payload_retorno ){
+         let pubret = await publisher(process.env.AMQP_WEBHOOK_QUEUE, payload_retorno );
+         console.log('pubret', pubret );
+    }
+
+    if ( process.env.AMQP_QUEUE_PUBLISHER && payload_retorno ){
+        let pubret = await publisher(process.env.AMQP_QUEUE_PUBLISHER, payload_retorno );         
+         console.log('pubret', pubret );
+    }
+
+    await channel.ack(msg);    
+    
+  } catch (error) {
+    console.log( 'error', error );
+    await channel.ack(msg);    
+  }
+
 }
+
+module.exports.publisher = publisher;
+
+module.exports.worker = (async (callback) => {
+
+    const connection = await amqplib.connect(AMQP_URL, "heartbeat=60");
+    const channel = await connection.createChannel();
+
+    channel.prefetch(10);    
+    process.once('SIGINT', async () => { 
+      console.log('got sigint, closing connection');
+      await channel.close();
+      await connection.close(); 
+      process.exit(0);
+    });
+
+    await channel.assertQueue(AMQP_QUEUE, {durable: true});
+    
+    await channel.consume(AMQP_QUEUE, 
+      async (payload) => {	
+        await processMessage(payload, channel, callback);        
+      },{
+      noAck: false/*,
+      consumerTag: 'email_consumer'*/
+    });
+    
+    console.log(" [*] Waiting for jobs... ");
+
+
+})();
---
+++ publisher.js
@@ -0,0 +1,53 @@
+const AMQP_URL = process.env.AMQP_URL;
+
+const amqplib = require('amqplib');
+const amqpUrl = AMQP_URL;
+
+module.exports = async (AMQP_QUEUE, msgs = null) => {
+  
+  if (!msgs || msgs.length == 0) return;
+
+  const connection = await amqplib.connect(amqpUrl, 'heartbeat=60');
+  const channel = await connection.createChannel();
+
+  var rets = [];
+
+  try {
+    
+    const exchange = AMQP_QUEUE+'_exchange';
+    const queue = AMQP_QUEUE;
+    const routingKey = AMQP_QUEUE+'_key';
+    
+    await channel.assertExchange(exchange, 'direct', {durable: true});
+    await channel.assertQueue(queue, {
+      durable: true,
+      autoDelete: false
+    });
+    await channel.bindQueue(queue, exchange, routingKey);		
+		
+    //converte sempre em array
+    msgs = ( Array.isArray( msgs ) ) ? msgs : [msgs];    
+
+    for (let msg of msgs) {
+      let ret = await channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(msg)));  
+      rets.push(ret);
+    };
+
+    console.log( rets );
+
+    
+    
+  } catch(e) {
+
+    rets = e
+
+  } finally { 
+
+    await channel.close();
+    await connection.close();
+
+    return rets;
+    
+  }
+  
+}
